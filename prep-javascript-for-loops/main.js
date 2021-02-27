5c48661f550aa791b19ee7e36621f75ecd9e8ccd
Steve Smith <Steve.A.Dore@outlook.com>
2021-02-26 17:03:48 -0800

    completed for loop exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,16 @@
+// create your loops here.
+
+// first loop
+for (let i = 2; i < 10; i++) {
+  console.log(i);
+}
+
+// second loop
+for (let j = 2; j < 20; j += 2) {
+  console.log(j);
+}
+
+// third loop
+for (let k = 100; k > 1; k--) {
+  console.log('Time until explosion: ', k);
+}
